cec570231dbe38219869be1a784d4cc760095210
Richard Broberg <rhb@brobasino.com>
2018-06-19 21:08:51 -0400

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => { // eslint-disable-line no-unused-vars// eslint-disable-line no-unused-vars
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
+    // socket.emit from Admin, text 'Welcome to the chat app'
+    // socket.broadcast.emit, from Admin, text 'New user joined'
     socket.on('createMessage', (message) => {
         console.log('message received: ', message);
         io.emit('newMessage', {
