359d9505693585b5d4c3678d1d58c14de358861b
Richard Broberg <rhb@brobasino.com>
2018-06-21 12:42:01 -0400

    jQuery added; form for messages and show incoming messages in browser

--- server/server.js
+++ server/server.js
@@ -18,9 +18,10 @@ io.on('connection', (socket) => { // eslint-disable-line no-unused-vars// eslint
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('message received: ', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('this is from the server');
     });
 
     socket.on('disconnect', (socket) => { // eslint-disable-line no-unused-vars
