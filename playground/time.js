7569b65e5dd9c6b7c9bef085c69945a79b57892a
Richard Broberg <rhb@brobasino.com>
2018-06-26 21:16:59 -0400

    silly playgrond file

---
+++ playground/time.js
@@ -0,0 +1,22 @@
+// // jan 1, 1970 00:00:00 am UTC = 0
+
+// const moment = require('moment');
+
+// //var date = new Date();
+
+// var months = ['Jan', 'Feb'];
+
+// // console.log(date.getMonth());
+
+// //var date = moment();
+// //date.add(100, 'year').subtract(9, 'months');
+// //console.log(date.format('MMM Do, YYYY'));
+
+// // 10:35 am padded minutes, unpadded hours, 12 hour clock, am/pm
+
+// var now = moment().valueOf();
+// console.log(now);
+
+// var createdAt = 1234;
+// const date = moment(createdAt);
+// console.log(date.format('h:mm a'));
